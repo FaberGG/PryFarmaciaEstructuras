d2f1487f0549a57366fe3ed5951d7f491bb33683
FaberGG <jhonfabernavia@gmail.com>
2024-05-20 00:05:39 -0500

    Primer Commit Estructura basica del proyecto

---
+++ src/main/java/com/farmacia/PryFarmaciaEstructuras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.farmacia;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class PryFarmaciaEstructuras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
